e9c8936173ab24fddd026bbaffe990062dc652f7
nia.team.developers <jobisaacmaina22@gmail.com>
2025-11-07 02:00:41 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bilared-cache-v1";
+const CACHE_NAME = "bilared-cache-v2";
 const urlsToCache = [
   "/",
   "/index.html",
@@ -6,21 +6,43 @@ const urlsToCache = [
   "/disclaimer.html",
   "/style.css",
   "/script.js",
+  "/howToPlay.js",
+  "/installPrompt.js",
+  "/playerCount.js",
+  "/notification.js"
   "/images/cat.png",
   "/images/cat-blue.png"
 ];
 
-// Install
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log("Caching app shell...");
+      //console.log("Caching app shell...");
       return cache.addAll(urlsToCache);
     })
   );
+  
+  self.skipWaiting();
 });
 
-// Fetch
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            //console.log("Deleting old cache:", name);
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+  
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
