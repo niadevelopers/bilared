35df2dd31239b53900c55cf81e07797a0c26fbaa
nia.team.developers <jobisaacmaina22@gmail.com>
2025-11-07 01:36:30 +0300

    Create playerCount.js

---
+++ playerCount.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", () => {
+  loadWallet();
+});
+
+const playerCountEl = document.getElementById("currentPlayers");
+function updatePlayerCount() {
+  const randomPlayers = Math.floor(200 + Math.random() * 250);
+  playerCountEl.textContent = `Current Players: ${randomPlayers.toLocaleString()}`;
+}
+setInterval(updatePlayerCount, 15000);
+updatePlayerCount();
