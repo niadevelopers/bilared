f09df00b5ca9a64f070707c2839f2b22d48dd654
nia.team.developers <jobisaacmaina22@gmail.com>
2025-11-08 21:36:04 +0300

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -11,7 +11,7 @@
 const playerCountEl = document.getElementById("currentPlayers");
 function updatePlayerCount() {
   const randomPlayers = Math.floor(200 + Math.random() * 250);
-  playerCountEl.textContent = `Current Players: ${randomPlayers.toLocaleString()}`;
+  playerCountEl.textContent = `Online: ${randomPlayers.toLocaleString()}`;
 }
 setInterval(updatePlayerCount, 15000);
 updatePlayerCount();
