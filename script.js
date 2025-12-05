b366fdd6788f5170ad8d3021ce33e395f6f844c7
nia.team.developers <jobisaacmaina22@gmail.com>
2025-12-05 21:40:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -431,7 +431,7 @@ depositBtn.onclick = async () => {
 
   const amountInput = await prompt("Enter deposit amount (KES):");
   const amount = parseFloat(amountInput);
-  if (!amount || isNaN(amount) || amount < 1 || amount > 150000) return alert("Enter valid amount (100–150,000).");
+  if (!amount || isNaN(amount) || amount < 100 || amount > 150000) return alert("Enter valid amount (100–150,000).");
 
   const email = await prompt("Enter your email:");
   if (!email || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) return alert("Enter a valid email.");
@@ -981,6 +981,7 @@ async function endGame(result){
 }
 
 startGameBtn.onclick=startGame;
+
 
 
 
