db91860f6731857330524e1848efb8addc4b69cd
nia.team.developers <jobisaacmaina22@gmail.com>
2025-12-05 21:30:39 +0300

    Update Payment.js

--- models/Payment.js
+++ models/Payment.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 const paymentSchema = new mongoose.Schema({
-  userId: String,
   providerRef: String,
+  email: String,
   amount: Number,
   status: String,
+  walletUpdated: { type: Boolean, default: false }, 
   createdAt: { type: Date, default: Date.now },
 });
 
